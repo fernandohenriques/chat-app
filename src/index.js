a3de1a4c87595276216e7b27c04b2ca4b9c2dd17
Fernando Henriques <fernando.trabalhos@gmail.com>
2018-09-02 13:08:42 -0300

    Add Styled Components and config Inject Global CSS

--- src/index.js
+++ src/index.js
@@ -1,7 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Routes from './config/Routes';
+import { injectGlobal } from 'styled-components';
 import registerServiceWorker from './registerServiceWorker';
+
+injectGlobal`
+  *, *::after, *::before {
+    margin: 0;
+    padding: 0;
+  }
+  body {
+    font-family: 'Roboto', sans-serif;
+    letter-spacing: 0.2px;
+  }
+`;
 
 const App = () => <Routes />;
 
