3e3a673d8787f151323b44c9db819a37d9cf09fb
Fernando Henriques <fernando.trabalhos@gmail.com>
2018-09-02 13:46:32 -0300

    NotFound Page configured

---
+++ src/components/pages/NotFound/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const NotFound = () => (
+  <div>
+    <p>
+      Page not found :( <br />
+      Maybe the page you are looking for has been removed, or you typed in the wrong URL.
+    </p>
+  </div>
+);
+
+export default NotFound;
--- src/config/Routes.js
+++ src/config/Routes.js
@@ -3,11 +3,13 @@ import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
 /* Pages */
 import Home from '../components/pages/Home';
+import NotFound from '../components/pages/NotFound';
 
 const Router = () => (
   <BrowserRouter>
     <Switch>
       <Route exact path='/' component={Home} />
+      <Route path='*' component={NotFound} />
     </Switch>
   </BrowserRouter>
 );
