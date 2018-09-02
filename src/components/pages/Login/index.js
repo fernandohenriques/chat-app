edd6f6ce7247164b6ed92ca8be90d6bc3c735407
Fernando Henriques <fernando.trabalhos@gmail.com>
2018-09-02 14:03:55 -0300

    Add PropTypes and Material-UI libs

---
+++ src/components/pages/Login/index.js
@@ -0,0 +1,15 @@
+import React, { Fragment } from 'react';
+import PropTypes from 'prop-types';
+
+
+const Login = () => {
+  return (
+    <Fragment>
+      <main className={classes.layout}>
+      </main>
+    </Fragment>
+  );
+};
+
+
+export default Login;
