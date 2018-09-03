d762a8dbbe8c4c06d835e5b34abdc93ffdc27ebe
Fernando Henriques <fernando.trabalhos@gmail.com>
2018-09-03 00:55:35 -0300

    Refatoring Login Page component to connect in Redux

--- src/components/pages/Login/index.js
+++ src/components/pages/Login/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 import Button from '@material-ui/core/Button';
@@ -7,47 +7,66 @@ import FormControl from '@material-ui/core/FormControl';
 import Input from '@material-ui/core/Input';
 import InputLabel from '@material-ui/core/InputLabel';
 import withStyles from '@material-ui/core/styles/withStyles';
+import { compose } from 'ramda';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
 
+import { setLoginUser } from '../../../store/ducks/user';
+import Api from '../../../services/api';
 import TemplateMain from '../../templates/Main';
 import Paper from '../../molecules/Paper';
 import SubmitButton from '../../atoms/SubmitButton';
 import styles from './styles';
 
+class Login extends Component {
+  constructor(props) {
+    super(props);
+  }
 
-const Login = (props) => {
-  const { classes } = props;
+  login() {
+    const { setLoginUser } = this.props;
+    //const data = await Api.getAllUsers(login);
+  }
 
-  return (
-    <TemplateMain>
-      <div className={classes.layout}>
-        <Paper title="Login" iconComponent={<LockIcon />}>
-          <form className={classes.form}>
+  render() {
+    const { classes } = this.props;
 
-            <FormControl margin="normal" required fullWidth>
-              <InputLabel htmlFor="email">Seu e-mail</InputLabel>
-              <Input id="email" name="email" autoComplete="email" autoFocus />
-            </FormControl>
+    return (
+      <TemplateMain>
+        <div className={classes.layout}>
+          <Paper title="Login" iconComponent={<LockIcon />}>
+            <form className={classes.form}>
 
-            <FormControl margin="normal" required fullWidth>
-              <InputLabel htmlFor="password">Sua senha</InputLabel>
-              <Input name="password" type="password" id="password" autoComplete="current-password" />
-            </FormControl>
+              <FormControl margin="normal" required fullWidth>
+                <InputLabel htmlFor="email">Seu e-mail</InputLabel>
+                <Input id="email" name="email" autoComplete="email" autoFocus />
+              </FormControl>
 
-            <SubmitButton fullWidth title="Entrar" />
-            <div className={classes.wrapperLink}>
-              <Button component={Link} to="/register">
-                {'Não sou Cadastrado'}
-              </Button>
-            </div>
-          </form>
-        </Paper>
-      </div>
-    </TemplateMain>
-  );
-};
+              <FormControl margin="normal" required fullWidth>
+                <InputLabel htmlFor="password">Sua senha</InputLabel>
+                <Input name="password" type="password" id="password" autoComplete="current-password" />
+              </FormControl>
+
+              <SubmitButton fullWidth title="Entrar" onClick={() => this.login()} />
+              <div className={classes.wrapperLink}>
+                <Button component={Link} to="/register">
+                  {'Não sou Cadastrado'}
+                </Button>
+              </div>
+            </form>
+          </Paper>
+        </div>
+      </TemplateMain>
+    );
+  }
+}
 
 Login.propTypes = {
+  setLoginUser: PropTypes.func.isRequired,
   classes: PropTypes.object.isRequired,
 };
 
-export default withStyles(styles)(Login);
+
+const mapDispatchToProps = dispatch => bindActionCreators({ setLoginUser }, dispatch);
+
+export default compose(withStyles(styles), connect(null, mapDispatchToProps))(Login);
