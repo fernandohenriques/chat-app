59dfd348df1ea9878f76585343f44cf6f5368608
Fernando Henriques <fernando.trabalhos@gmail.com>
2018-09-03 19:17:11 -0300

    Create action to update contact status

--- src/store/ducks/contacts.js
+++ src/store/ducks/contacts.js
@@ -1,3 +1,4 @@
+import * as R from 'ramda';
 import { createActions, createReducer } from 'reduxsauce';
 
 const initialState = [];
@@ -7,8 +8,7 @@ const { Types, Creators } = createActions({
   updateContacts: ['contacts', 'emailLogged'],
   setStatus: ['id','status'],
 });
-const { updateContacts } = Creators;
-
+const { updateContacts, setStatus } = Creators;
 
 /* Reducer Handlers */
 const update = (state = initialState, action) => {
@@ -16,8 +16,17 @@ const update = (state = initialState, action) => {
   return contacts.filter(c => c.email !== emailLogged);
 };
 
+const updateStatus = (state = initialState, action) => {
+  const user = R.find(R.propEq('_id', action.id))(state);
+  const newState = state.filter(u => u._id != action.id);
+  user[0].online = action.status;
+  newState.push(user);
+  return newState;
+};
+
 const handlers = {
   [Types.UPDATE_CONTACTS]: update,
+  [Types.SET_STATUS]: updateStatus,
 };
 
 /* Create Reducer */
