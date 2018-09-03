96812fe1d8c5df7c12f0b23e6afe5ab0619e134e
Fernando Henriques <fernando.trabalhos@gmail.com>
2018-09-03 01:12:59 -0300

    Redirect to Home if not logged

--- src/components/pages/Home/index.js
+++ src/components/pages/Home/index.js
@@ -1,24 +1,41 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import withStyles from '@material-ui/core/styles/withStyles';
+import { compose } from 'ramda';
+import { connect } from 'react-redux';
+import { Redirect } from 'react-router-dom';
 
 import TemplateMainLogged from '../../templates/MainLogged';
 import styles from './styles';
 
-const Home = (props) => {
-  const { classes } = props;
+class Home extends Component {
+  constructor(props) {
+    super(props);
+  }
 
-  return (
-    <TemplateMainLogged>
-      <div className={classes.body}>
-        Teste
-      </div>
-    </TemplateMainLogged>
-  );
-};
+  render() {
+    const { classes, logged } = this.props;
+
+    if (!logged)
+      return <Redirect to="/login" />;
+
+    return (
+      <TemplateMainLogged>
+        <div className={classes.body}>
+          Teste
+        </div>
+      </TemplateMainLogged>
+    );
+
+  }
+}
 
 Home.propTypes = {
+  logged: PropTypes.bool.isRequired,
   classes: PropTypes.object.isRequired,
 };
 
-export default withStyles(styles)(Home);
+
+const mapStateToProps = store => ({ logged: store.user.logged, });
+
+export default compose(withStyles(styles), connect(mapStateToProps))(Home);
